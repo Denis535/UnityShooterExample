eae2f74fa238564c8536435a02e5282bbb880e39
Denis <dddenisss35@gmail.com>
2024-12-01 11:11:56 +0200

    Asset Store

---
+++ UnityShooterExample/Assets/Plugins/Denis535.CleanArchitectureGameFramework.Additions/UnityEngine.Framework_/01.UI/04.UIView/UIRootWidgetViewBase.cs
@@ -0,0 +1,96 @@
+﻿#nullable enable
+namespace UnityEngine.Framework {
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using UnityEngine;
+    using UnityEngine.UIElements;
+
+    public abstract class UIRootWidgetViewBase : UIViewBase {
+
+        // Views
+        public IEnumerable<UIViewBase> Views => Children().Cast<UIViewBase>();
+        // OnSubmit
+        public event EventCallback<NavigationSubmitEvent> OnSubmitEvent {
+            add => RegisterCallback( value, TrickleDown.TrickleDown );
+            remove => UnregisterCallback( value, TrickleDown.TrickleDown );
+        }
+        public event EventCallback<NavigationCancelEvent> OnCancelEvent {
+            add => RegisterCallback( value, TrickleDown.TrickleDown );
+            remove => UnregisterCallback( value, TrickleDown.TrickleDown );
+        }
+
+        // Constructor
+        public UIRootWidgetViewBase() {
+            name = "root-widget-view";
+            AddToClassList( "root-widget-view" );
+            pickingMode = PickingMode.Ignore;
+        }
+        public override void Dispose() {
+            base.Dispose();
+        }
+
+        // AddView
+        protected internal override bool AddView(UIViewBase view) {
+            Assert.Argument.Message( $"Argument 'view' ({view}) must be non-disposed" ).Valid( !view.IsDisposed );
+            Assert.Argument.Message( $"Argument 'view' ({view}) must be non-attached to parent" ).Valid( !view.IsAttachedToParent );
+            Assert.Operation.Message( $"View {this} must be non-disposed" ).NotDisposed( !IsDisposed );
+            Add( view );
+            Sort();
+            SetVisibility( (IReadOnlyList<VisualElement>) Children() );
+            return true;
+        }
+        protected internal override bool RemoveView(UIViewBase view) {
+            Assert.Argument.Message( $"Argument 'view' ({view}) must be non-disposed" ).Valid( !view.IsDisposed );
+            Assert.Argument.Message( $"Argument 'view' ({view}) must be attached to parent" ).Valid( view.IsAttachedToParent );
+            Assert.Operation.Message( $"View {this} must be non-disposed" ).NotDisposed( !IsDisposed );
+            Remove( view );
+            SetVisibility( (IReadOnlyList<VisualElement>) Children() );
+            return true;
+        }
+
+        // Sort
+        protected virtual void Sort() {
+            Sort( (a, b) => Comparer<int>.Default.Compare( GetOrderOf( (UIViewBase) a ), GetOrderOf( (UIViewBase) b ) ) );
+        }
+        protected virtual int GetOrderOf(UIViewBase view) {
+            return 0;
+        }
+
+        // SetVisibility
+        protected virtual void SetVisibility(IReadOnlyList<VisualElement> views) {
+            foreach (var view in views.Cast<UIViewBase>().SkipLast( 1 )) {
+                if (view.HasFocusedElement()) {
+                    view.SaveFocus();
+                }
+            }
+            for (var i = 0; i < views.Count; i++) {
+                var view = (UIViewBase) views[ i ];
+                var next = (UIViewBase) views.ElementAtOrDefault( i + 1 );
+                SetVisibility( view, next );
+            }
+            if (views.Any()) {
+                var view = (UIViewBase) views.Last();
+                if (!view.HasFocusedElement()) {
+                    if (!view.LoadFocus()) {
+                        view.InitFocus();
+                    }
+                }
+            }
+        }
+        protected virtual void SetVisibility(UIViewBase view, UIViewBase? next) {
+            if (next != null) {
+                view.SetEnabled( false );
+                view.SetDisplayed( GetLayerOf( view ) != GetLayerOf( next ) );
+            } else {
+                view.SetEnabled( true );
+                view.SetDisplayed( true );
+            }
+        }
+        protected virtual int GetLayerOf(UIViewBase view) {
+            return 0;
+        }
+
+    }
+}
---
+++ UnityShooterExample/Assets/Plugins/Denis535.CleanArchitectureGameFramework.Internal/System.Diagnostics.CodeAnalysis/NullableAttribute.cs
@@ -0,0 +1,106 @@
+#nullable enable
+namespace System.Diagnostics.CodeAnalysis {
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+
+    // https://learn.microsoft.com/en-us/dotnet/api/system.diagnostics.codeanalysis
+
+    // Input:  field/property setter/method argument
+    // Output: field/property getter/method result (or out argument)
+
+    // AllowNull            - Allows null input
+    // DisallowNull         - Disallows null input
+
+    // MaybeNull            - Output maybe null
+    // MaybeNull/When       - Output maybe null (when result == true/false)
+
+    // NotNull              - Output is not null
+    // NotNull/When         - Output is not null (when result == true/false)
+    // NotNull/If/NotNull   - Output is not null (if argument != null)
+
+    // MemberNotNull        - Member output is not null
+    // MemberNotNull/When   - Member output is not null (when result == true/false)
+
+    // DoesNotReturn        - Throws exception
+    // DoesNotReturn/If     - Throws exception (if argument == true/false)
+
+    // AllowNull
+    //[AttributeUsage( AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property, Inherited = false )]
+    //public sealed class AllowNullAttribute : Attribute {
+    //}
+    //[AttributeUsage( AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property, Inherited = false )]
+    //public sealed class DisallowNullAttribute : Attribute {
+    //}
+
+    // MaybeNull
+    //[AttributeUsage( AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, Inherited = false )]
+    //public sealed class MaybeNullAttribute : Attribute {
+    //}
+    //[AttributeUsage( AttributeTargets.Parameter, Inherited = false )]
+    //public sealed class MaybeNullWhenAttribute : Attribute {
+    //    public bool ReturnValue { get; }
+    //    public MaybeNullWhenAttribute(bool returnValue) {
+    //        ReturnValue = returnValue;
+    //    }
+    //}
+
+    // NotNull
+    //[AttributeUsage( AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, Inherited = false )]
+    //public sealed class NotNullAttribute : Attribute {
+    //}
+    //[AttributeUsage( AttributeTargets.Parameter, Inherited = false )]
+    //public sealed class NotNullWhenAttribute : Attribute {
+    //    public bool ReturnValue { get; }
+    //    public NotNullWhenAttribute(bool returnValue) {
+    //        ReturnValue = returnValue;
+    //    }
+    //}
+    //[AttributeUsage( AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, AllowMultiple = true, Inherited = false )]
+    //public sealed class NotNullIfNotNullAttribute : Attribute {
+    //    public string ParameterName { get; }
+    //    public NotNullIfNotNullAttribute(string parameterName) {
+    //        ParameterName = parameterName;
+    //    }
+    //}
+
+    // MemberNotNull
+    [EditorBrowsable( EditorBrowsableState.Never )]
+    [AttributeUsage( AttributeTargets.Method | AttributeTargets.Property, Inherited = false, AllowMultiple = true )]
+    public sealed class MemberNotNullAttribute : Attribute {
+        public string[] Members { get; }
+        public MemberNotNullAttribute(string member) {
+            Members = new[] { member };
+        }
+        public MemberNotNullAttribute(params string[] members) {
+            Members = members;
+        }
+    }
+    [EditorBrowsable( EditorBrowsableState.Never )]
+    [AttributeUsage( AttributeTargets.Method | AttributeTargets.Property, Inherited = false, AllowMultiple = true )]
+    public sealed class MemberNotNullWhenAttribute : Attribute {
+        public bool ReturnValue { get; }
+        public string[] Members { get; }
+        public MemberNotNullWhenAttribute(bool returnValue, string member) {
+            ReturnValue = returnValue;
+            Members = new[] { member };
+        }
+        public MemberNotNullWhenAttribute(bool returnValue, params string[] members) {
+            ReturnValue = returnValue;
+            Members = members;
+        }
+    }
+
+    // DoesNotReturn
+    //[AttributeUsage( AttributeTargets.Method, Inherited = false )]
+    //public sealed class DoesNotReturnAttribute : Attribute {
+    //}
+    //[AttributeUsage( AttributeTargets.Parameter, Inherited = false )]
+    //public sealed class DoesNotReturnIfAttribute : Attribute {
+    //    public bool ParameterValue { get; }
+    //    public DoesNotReturnIfAttribute(bool parameterValue) {
+    //        ParameterValue = parameterValue;
+    //    }
+    //}
+}
